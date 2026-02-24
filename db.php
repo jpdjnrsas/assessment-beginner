579705be7c2d332a4ce4227266a66ed0b36094a1
jpdjnrsas <diuyan7@gmail.com>
2026-02-24 14:49:52 +0800

    Service Management (List + Edit)

--- db.php
+++ db.php
@@ -7,5 +7,5 @@ $dbname = "assessment_db";
 $conn = mysqli_connect($host, $user, $pass, $dbname);
 
 if (!$conn) {
-  die("Database connection failed: " . mysqli_connect_error());
+    die("Database connection failed: " . mysqli_connect_error());
 }
